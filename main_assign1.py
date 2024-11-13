ca7764936ff0955bd8fefcf5f9650f3dc9abfbf5
Lukas Loidolt <e1634039@student.tuwien.ac.at>
2024-11-13 21:58:48 +0100

    use forward slashes for file paths
    modern windows should be able to handle those. But Mac/Linux can't handle backward slashes

--- main_assign1.py
+++ main_assign1.py
@@ -5,8 +5,8 @@ from assignment1.road_safety_prep import CheckDatasetCondition
 
 if __name__ == '__main__':
     WORKING_DIR: str = os.getcwd()
-    PHISHING_RAW_DATA_PATH: str = WORKING_DIR + "\\assignment0\\data\\PhiUSIIL_Phishing_URL_Dataset.csv"
-    ROAD_SAFETY_RAW_DATA_PATH: str = WORKING_DIR + "\\assignment1\\data\\road_safety_dataset.arff"
+    PHISHING_RAW_DATA_PATH: str = WORKING_DIR + "/assignment0/data/PhiUSIIL_Phishing_URL_Dataset.csv"
+    ROAD_SAFETY_RAW_DATA_PATH: str = WORKING_DIR + "/assignment1/data/road_safety_dataset.arff"
 
     phishing_data: pd.DataFrame = process_phishing_data(PHISHING_RAW_DATA_PATH)
     road_safety_data: pd.DataFrame = process_road_safety_data(ROAD_SAFETY_RAW_DATA_PATH)
