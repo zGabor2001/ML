83e204a252481bd3091531da62a3d0e036440099
zGabor2001 <zgabor2001@gmail.com>
2024-12-14 15:56:08 +0100

    fix string leak issue

--- assignment2/toronto_rental.py
+++ assignment2/toronto_rental.py
@@ -21,9 +21,8 @@ _OUTPUT_HYPERPARAMETERS_RESULTS = _OUTPUT_HYPERPARAMETERS_FOLDER / 'results.csv'
 def explore_toronto_rental_dataset():
     df = load_dataset(_DATASET_ID, _DATASET_PATH)
     df = df.iloc[:, 1:]
+    df['Price'] = df['Price'].str.replace(',', '').astype(float)    # Is this a good idea???
     x_train, x_test, y_train, y_test = get_train_test_data(df=df, target=_TARGET_VARIABLE, split_size=_TEST_SPLIT_SIZE)
-
-    df['Price'] = df['Price'].str.replace(',', '').astype(float)
 
     address_preprocessing_pipeline = Pipeline([
         ('ordinal_encoded', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1))
@@ -32,7 +31,7 @@ def explore_toronto_rental_dataset():
     preprocessing_pipeline = Pipeline([
         ('column transformations', ColumnTransformer([
             ('address', address_preprocessing_pipeline, ['Address'])
-        ]))
+        ], remainder='passthrough', verbose_feature_names_out=False))
     ])
 
     x_train_transformed = preprocessing_pipeline.fit_transform(x_train)
@@ -43,7 +42,7 @@ def explore_toronto_rental_dataset():
         no_of_trees=[50, 100, 200],
         max_depth=[20, 50, 500],
         min_samples=[10, 100, 200],
-        feature_subset_size=[4, 9, 14],
+        feature_subset_size=[2, 3, 5],
     )
 
     results = run_random_forest_with_varied_params(
