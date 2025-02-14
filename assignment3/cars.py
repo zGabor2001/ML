7532611f0cbef579ddfe824e98d5cff4ce764f5e
gabi-mac <>
2025-02-14 14:51:08 +0100

    add car dataset preparation and initial processing pipeline

---
+++ assignment3/cars.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import pandas as pd
+from sklearn import set_config
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
+
+from util.data_utils import load_dataset, get_train_test_data, timer
+
+_DATASET_ID = 44994
+_DATASET_PATH = 'data/cars.csv'
+_TEST_SPLIT_SIZE = 0.2
+_TARGET_VARIABLE = 'Price'
+_CORRELATION_DROP_THRESHOLD = 1.0
+_TEST_RUN = False
+
+
+_OUTPUT_FOLDER = Path('output/cars')
+_OUTPUT_HYPERPARAMETERS_FOLDER = _OUTPUT_FOLDER / 'parameter_permutation'
+_OUTPUT_HYPERPARAMETERS_RESULTS = _OUTPUT_HYPERPARAMETERS_FOLDER / 'results.csv'
+
+_OUTPUT_KNN = _OUTPUT_FOLDER / 'knn'
+_OUTPUT_KNN_HYPERPARAMETER_PERMUTATIONS = _OUTPUT_KNN / 'parameter_permutations.csv'
+
+
+def prepare_cars_dataset():
+    df = load_dataset(_DATASET_ID, _DATASET_PATH)
+
+    
+
+    # data split into features and target variable
+    # as well as into training and testing sets
+    x_train, x_test, y_train, y_test = get_train_test_data(df=df, target=_TARGET_VARIABLE, split_size=_TEST_SPLIT_SIZE)
+
+    
+    return y_train, y_test
+
+prepare_cars_dataset()
