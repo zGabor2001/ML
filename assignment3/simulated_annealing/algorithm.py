d366058a627f7a37eebf63590843666592f56504
Lukas Loidolt <e1634039@student.tuwien.ac.at>
2025-02-24 00:54:31 +0100

    simulated annealing: add docs

--- assignment3/simulated_annealing/algorithm.py
+++ assignment3/simulated_annealing/algorithm.py
@@ -12,6 +12,80 @@ from assignment3.simulated_annealing.solution import CandidateSolution
 
 
 class SimulatedAnnealing:
+    """
+    Simulated Annealing algorithm for automatic model selection and hyperparameter tuning.
+
+    Parameters
+    ----------
+    model_configs : list[ModelConfig]
+        A list of model configurations to be tested by the algorithm.
+        A model configuration is a dataclass with the following attributes:
+        - name: str
+            A human-readable name for the model configuration.
+        - model_cls: Type[BaseRegressor]
+            The class of the model to be instantiated and trained.
+        - parameters: dict[str, list[any]]
+            A dictionary with the names of the hyperparameters as keys and a list of possible values as values.
+        - training_device: str, optional
+            The device to be used for training the model.
+
+    x_train : np.ndarray
+        The input features of the training dataset.
+
+    y_train : np.ndarray
+        The target values of the training dataset.
+
+    x_test : np.ndarray
+        The input features of the test dataset.
+
+    y_test : np.ndarray
+        The target values of the test dataset.
+
+    min_temp : float, optional
+        The minimum temperature for the annealing process. Set to 0.001 * initial_temperature by default.
+
+    max_time : timedelta, optional
+        The maximum time allowed for the algorithm to run. If not specified, the algorithm will run until min_temp or max_iter is reached.
+
+    max_steps : int, optional
+        The maximum number of steps (iterations) allowed for the algorithm to run. If not specified, the algorithm will run until min_temp or max_time is reached.
+
+    p_test_different_model : float, optional
+        The probability of testing a solution from a different model configuration on each iteration. Default is 0.25.
+
+    neighbor_range : float, optional
+        The range of the neighborhood search for each hyperparameter. Default is 0.25.
+
+    initial_temperature : float, optional
+        The initial temperature for the annealing process. If not specified, it will be calculated based on the initial acceptance rate.
+
+    initial_acceptance_rate : float, optional
+        The initial acceptance rate for the annealing process. Default is 0.8.
+        This parameter is used to calculate the initial temperature if it is not specified.
+
+    cooling_factor : float, optional
+        The cooling factor for the annealing process. Default is 0.95.
+
+    iterations_per_step : int, optional
+        The number of iterations to perform at each step of the annealing process. Default is 10.
+
+    Attributes
+    ----------
+    best_solution : CandidateSolution
+        The best solution found by the algorithm.
+
+    solutions_history_dict : list[dict]
+        A list of dictionaries representing the history of solutions found by the algorithm.
+
+    solutions_history_df : pd.DataFrame
+        A DataFrame representing the history of solutions found by the algorithm.
+
+    Methods
+    -------
+    run()
+        Run the simulated annealing algorithm and return the best solution found.
+    """
+
     def __init__(
             self,
             model_configs: list[ModelConfig],
@@ -21,13 +95,13 @@ class SimulatedAnnealing:
             y_test: np.ndarray,
             min_temp: float | None = None,
             max_time: timedelta | None = None,
-            max_iter: int | None = None,
+            max_steps: int | None = None,
             p_test_different_model: float = 0.25,
             neighbor_range: float = 0.25,
             initial_temperature: float | None = None,
             initial_acceptance_rate: float = 0.8,
             cooling_factor: float = 0.95,
-            iterations_per_temp: int = 100,
+            iterations_per_step: int = 10,
     ):
         self.model_configs = model_configs
         self.x_train = x_train
@@ -36,16 +110,16 @@ class SimulatedAnnealing:
         self.y_test = y_test
         self.min_temp = min_temp
         self.max_time = max_time.total_seconds() if max_time is not None else None
-        self.max_iter = max_iter
+        self.max_steps = max_steps
         self.p_test_different_model = p_test_different_model
         self.neighbor_range = neighbor_range
         self.initial_temperature = initial_temperature
         self.initial_acceptance_rate = initial_acceptance_rate
         self.cooling_factor = cooling_factor
-        self.iterations_per_temp = iterations_per_temp
+        self.iterations_per_step = iterations_per_step
         self.best_solution: CandidateSolution | None = None
         self._start_time: float = 0.0
-        self._tick: int = 0
+        self._step: int = 0
         self._model_solutions: dict[ModelConfig, CandidateSolution] = {}
         self._current_model: ModelConfig | None = None
         self._previous_solutions: list[dict[str, any]] = []
@@ -66,7 +140,7 @@ class SimulatedAnnealing:
     def run(self) -> CandidateSolution:
         logging.info("Starting simulated annealing ...")
         self._start_time = time.time()
-        self._tick = 0
+        self._step = 1
         self._model_solutions = {
             config: CandidateSolution.from_model_config(config, self.x_train, self.y_train, self.x_test, self.y_test)
             for config in self.model_configs
@@ -81,9 +155,9 @@ class SimulatedAnnealing:
             self.min_temp = 0.001 * self._temperature
 
         while not self._halting_condition():
-            logging.info(f"Tick: {self._tick}, Temperature: {self._temperature:.2f}")
-            for i in range(self.iterations_per_temp):
-                logging.debug(f"Iteration {i} at tick {self._tick}")
+            logging.info(f"Step: {self._step}, Temperature: {self._temperature:.2f}")
+            for i in range(self.iterations_per_step):
+                logging.debug(f"Iteration {i} at step {self._step}")
                 neighbor = self._get_neighboring_solution()
                 logging.debug(f"Generated new solution: {neighbor.to_dict()}")
                 delta = self._calculate_solutions_delta(neighbor)
@@ -96,7 +170,7 @@ class SimulatedAnnealing:
                         self.best_solution = neighbor
                         logging.debug(f"New best solution: {neighbor.to_dict()}")
             self._temperature = self._temperature * self.cooling_factor
-            self._tick += 1
+            self._step += 1
         logging.info(f"Halting condition reached on final solution: {self.current_solution.to_dict()}")
         return self.best_solution
 
@@ -118,7 +192,7 @@ class SimulatedAnnealing:
         if self._temperature < self.min_temp:
             logging.debug("Minimum temperature reached. Halting now ...")
             return True
-        if self.max_iter is not None and self._tick >= self.max_iter:
+        if self.max_steps is not None and self._step > self.max_steps:
             logging.debug("Maximum iterations reached. Halting now ...")
             return True
         if self.max_time is not None and time.time() - self._start_time >= self.max_time:
--- assignment3/simulated_annealing/config.py
+++ assignment3/simulated_annealing/config.py
@@ -3,8 +3,23 @@ from typing import Type
 
 from assignment3.model.base_model import BaseRegressor
 
+
 @dataclass(frozen=True)
 class ModelConfig:
+    """
+    Configuration for a model to be trained
+
+    Attributes
+    ----------
+    name : str
+        A human-readable name for the model configuration.
+    model_cls : Type[BaseRegressor]
+        The class of the model to be instantiated and trained.
+    parameters : dict[str, list[any]]
+        A dictionary with the names of the hyperparameters as keys and a list of possible values as values.
+    training_device : str, optional
+        The device to be used for training the model.
+    """
     name: str
     model_cls: Type[BaseRegressor]
     parameters: dict[str, list[any]]
